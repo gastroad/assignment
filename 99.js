2470d10839703a4768b9ac02c7d66a340abd0a63
gastroad <gostroad37@gmail.com>
2017-08-02 09:32:10 +0900

    문자열다루기 기본

--- 99.js
+++ 99.js
@@ -1,30 +1,28 @@
 function alpha_string46(s){
   var result;
   // 함수를 완성하세요
+let a = s.split("")
+s *= 1;
+    if (typeof(s) === 'string' ) {
+    return  false
+    }
+    else{
+      switch (a.length){
+        case 4:
+        return true
+        break;
+        case 6:
+        return true
+        break;
+        default:
+        return false
+        break;
+      }
 
-
-
-    if (isNaN(s)) {
-    result = false
-
-  }
-  else{
-    switch (s.length){
-      case 4:
-      result = true
-      break;
-      case 6:
-      result = true
-      break;
-      default:
-      result = false
-      break;
     }
 
-  }
-return result
 }
 
 
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
-console.log( alpha_string46("a234") );
+console.log( alpha_string46("1234") );
