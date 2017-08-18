fb28c55b8316a4a12d11dae212753c3f01cf7c7f
gastroad <gostroad37@gmail.com>
2017-08-18 14:43:22 +0900

    라이프게임

--- ex.js
+++ ex.js
@@ -1,5 +1,8 @@
 const X = 0 // DEAD
 const O = 1 // ALIVE
+let lifeCell = 0;
+let deadCell = new Array();
+let aliveCell = new Array();
 
 let initialState = [
 	[X,X,X,X,X],
@@ -7,37 +10,156 @@ let initialState = [
 	[X,O,O,O,X],
 	[X,X,X,X,X],
 	[X,X,X,X,X],
-
 ]
 
 let secondState = [
 	[X,X,X,X,X],
-	[X,X,O,X,X],
-	[X,X,O,X,X],
-	[X,X,O,X,X],
+	[X,X,X,X,X],
+	[X,O,O,O,X],
+	[X,X,X,X,X],
 	[X,X,X,X,X],
 ]
 
-function findcell(){
-	let find = new Array()
-	for(let i =0; i<initialState.length;i++){
-		for(let j=0; j<initialState[0].length;j++){
-				if (initialState[i][j] === O){
-					find.push([i,j])
+function checkedDead(i, j) {
+	if (i === 0) {
+		if (j === 0) {
+			if (initialState[i][j + 1] === O) lifeCell++;
 
-				}
+			if (initialState[i + 1][j] === O) lifeCell++;
+			if (initialState[i + 1][j + 1] === O) lifeCell++;
+		} else if (j + 1 === initialState[0].legnth) {
+			if (initialState[i][j - 1] === O) lifeCell++;
+
+			if (initialState[i + 1][j - 1] === O) lifeCell++;
+			if (initialState[i + 1][j] === O) lifeCell++;
+		} else {
+			if (initialState[i][j - 1] === O) lifeCell++;
+			if (initialState[i][j + 1] === O) lifeCell++;
+
+			if (initialState[i + 1][j - 1] === O) lifeCell++;
+			if (initialState[i + 1][j] === O) lifeCell++;
+			if (initialState[i + 1][j + 1] === O) lifeCell++;
 		}
+
+	} else if (i + 1 === initialState.length) {
+		if (j === 0) {
+			if (initialState[i - 1][j] === O) lifeCell++;
+			if (initialState[i - 1][j + 1] === O) lifeCell++;
+
+			if (initialState[i][j + 1] === O) lifeCell++;
+		} else if (j + 1 === initialState[0].legnth) {
+			if (initialState[i - 1][j - 1] === O) lifeCell++;
+			if (initialState[i - 1][j] === O) lifeCell++;
+
+			if (initialState[i][j - 1] === O) lifeCell++;
+		} else {
+			if (initialState[i - 1][j - 1] === O) lifeCell++;
+			if (initialState[i - 1][j] === O) lifeCell++;
+			if (initialState[i - 1][j + 1] === O) lifeCell++;
+
+			if (initialState[i][j - 1] === O) lifeCell++;
+			if (initialState[i][j + 1] === O) lifeCell++;
+		}
+	} else {
+		if (initialState[i - 1][j - 1] === O) lifeCell++;
+		if (initialState[i - 1][j] === O) lifeCell++;
+		if (initialState[i - 1][j + 1] === O) lifeCell++;
+
+		if (initialState[i][j - 1] === O) lifeCell++;
+		if (initialState[i][j + 1] === O) lifeCell++;
+
+		if (initialState[i + 1][j - 1] === O) lifeCell++;
+		if (initialState[i + 1][j] === O) lifeCell++;
+		if (initialState[i + 1][j + 1] === O) lifeCell++;
 	}
 
-return find
+	if (lifeCell < 2 || lifeCell > 3) { deadCell.push([i, j]) }
+}
+
+function checkedAlive(i, j) {
+	if (i === 0) {
+		if (j === 0) {
+			if (initialState[i][j + 1] === O) lifeCell++;
+
+			if (initialState[i + 1][j] === O) lifeCell++;
+			if (initialState[i + 1][j + 1] === O) lifeCell++;
+		} else if (j + 1 === initialState[0].legnth) {
+			if (initialState[i][j - 1] === O) lifeCell++;
+
+			if (initialState[i + 1][j - 1] === O) lifeCell++;
+			if (initialState[i + 1][j] === O) lifeCell++;
+		} else {
+			if (initialState[i][j - 1] === O) lifeCell++;
+			if (initialState[i][j + 1] === O) lifeCell++;
+
+			if (initialState[i + 1][j - 1] === O) lifeCell++;
+			if (initialState[i + 1][j] === O) lifeCell++;
+			if (initialState[i + 1][j + 1] === O) lifeCell++;
+		}
+
+	} else if (i + 1 === initialState.length) {
+		if (j === 0) {
+			if (initialState[i - 1][j] === O) lifeCell++;
+			if (initialState[i - 1][j + 1] === O) lifeCell++;
+
+			if (initialState[i][j + 1] === O) lifeCell++;
+		} else if (j + 1 === initialState[0].legnth) {
+			if (initialState[i - 1][j - 1] === O) lifeCell++;
+			if (initialState[i - 1][j] === O) lifeCell++;
+
+			if (initialState[i][j - 1] === O) lifeCell++;
+		} else {
+			if (initialState[i - 1][j - 1] === O) lifeCell++;
+			if (initialState[i - 1][j] === O) lifeCell++;
+			if (initialState[i - 1][j + 1] === O) lifeCell++;
+
+			if (initialState[i][j - 1] === O) lifeCell++;
+			if (initialState[i][j + 1] === O) lifeCell++;
+		}
+	} else {
+		if (initialState[i - 1][j - 1] === O) lifeCell++;
+		if (initialState[i - 1][j] === O) lifeCell++;
+		if (initialState[i - 1][j + 1] === O) lifeCell++;
+
+		if (initialState[i][j - 1] === O) lifeCell++;
+		if (initialState[i][j + 1] === O) lifeCell++;
+
+		if (initialState[i + 1][j - 1] === O) lifeCell++;
+		if (initialState[i + 1][j] === O) lifeCell++;
+		if (initialState[i + 1][j + 1] === O) lifeCell++;
+	}
+
+
+	if (lifeCell === 3) { aliveCell.push([i, j]) }
+
+	lifeCell = 0;
 }
 
 function calculateNextState(prevState) {
+
         let nextState
 
 
+
+				for(let i=0; i < initialState.length; i++){
+					for (let j = 0; j < initialState[0].length; j++) {
+						// deadCell
+						if (initialState[i][j] === O) {
+							checkedDead(i, j);
+							continue;
+						}
+						// alive
+						checkedAlive(i, j)
+					}
+				}
+			console.log(deadCell);
+			console.log(aliveCell);
+
+				// killCell(deadCell);
+
         // write your code
-	return nextState
+	return
+
 }
 
 const str = JSON.stringify
